e8d9690efe5cc31f739b8773f912fc4f8b46f4bb
Kimtaegyu <rlaxorbsp@naver.com>
2022-07-06 15:50:21 +0900

    css animation

---
+++ main.js
@@ -0,0 +1,17 @@
+let mainText = document.querySelector("h1")
+
+window.addEventListener('scroll',function(){
+    let value = window.scrollY
+    console.log("scrollY", value);
+
+    if(value>300){
+        mainText.style.animation="disapper 1s ease-out forwards";
+    }else{
+        mainText.style.animation="slide 1s ease-out"
+    }
+})
+
+//addEventListener는 이벤트를 발생시키는걸로
+//addEventListener('event',(function))으로 정의한다
+
+//20220706 css스타일 애니메이션효과 공부
